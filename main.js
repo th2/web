002de2f0360eb7599951af7aca0ec216a4647f93
th2 <th2@users.noreply.github.com>
2020-02-28 23:00:35 +0100

    log via winston

--- main.js
+++ main.js
@@ -1,19 +1,28 @@
 'use strict'
 var bodyParser = require('body-parser')
 var express = require('express')
-var app = express()
-var config = require('./config/main.json')
+var expressWinston = require('express-winston')
+var winston = require('winston')
+require('winston-daily-rotate-file')
 var router = require('./router')
-var logger = require('./logger')
+var config = require('./config/main.json')
+var app = express()
 
-app.use(function (req, res, next) {
-  logger.visitor(req, 0)
-  next()
+var visitTransport = new (winston.transports.DailyRotateFile)({
+  filename: 'log/visit/%DATE%.log',
+  datePattern: 'YYYY-MM-DD'
 })
+var errorTransport = new (winston.transports.DailyRotateFile)({
+  filename: 'log/error/%DATE%.log',
+  datePattern: 'YYYY-MM-DD'
+})
+
+app.use(expressWinston.logger({ transports: [visitTransport] }))
 app.use(express.static('site'))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/', router)
+app.use(expressWinston.errorLogger({ transports: [errorTransport] }))
 
 module.exports.server = app.listen(config.mainPort, function () {
   console.log('listening on port ' + config.mainPort)
