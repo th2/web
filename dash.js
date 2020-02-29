807b57cde11179be7492288066953b8c081e81b9
th2 <th2@users.noreply.github.com>
2020-02-29 19:01:02 +0100

    dash: add aggreation table and bootstrap

--- dash.js
+++ dash.js
@@ -18,20 +18,31 @@ router.get('/', function (req, res) {
 router.get('/day/:day', function (req, res) {
   let page = `<html><head>
   <title>Dash ${req.params.day}</title>
-  <link rel="stylesheet" type="text/css" href="/dash.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="/dash.css">
+  <script src="sortabletable.js" language="javascript" type="text/javascript"></script>
+  <script src="tfAdapter.sortabletable.js" language="javascript" type="text/javascript"></script>
   </head><body>`
   page += generateVisitTable(req.params.day)
-  page += '<body></html>'
+  page += '<body class=".bg-dark"></html>'
   res.send(page)
 })
 
 function generateVisitTable(day) {
   const lines = fs.readFileSync(`log/visit/${day}`).toString().split("\n")
-  let table = '<table>'
+  let pathmap = new Map()
+  let sourcetable = `<table class="table table-dark table-striped"><tr>
+    <th>timestamp</th>
+    <th>level</th>
+    <th>message</th>
+    <th>meta.req</th>
+    <th>meta.res</td>
+    </tr>`
   for (let i in lines) {
     if (lines[i].startsWith('{')) {
       let visit = JSON.parse(lines[i])
-      table += `<tr>
+      pathmap.set(visit.message, pathmap.get(visit.message) + 1 || 1)
+      sourcetable += `<tr>
       <td>${visit.timestamp}</td>
       <td>${visit.level}</td>
       <td>${visit.message}</td>
@@ -40,8 +51,25 @@ function generateVisitTable(day) {
       </tr>`
     }
   }
-  table += '</table>'
-  return table
+
+  let pathtable = `<table class="table table-dark table-striped">
+    <thead>
+    <tr>
+    <th>path</th>
+    <th>count</th>
+    </tr>
+    </thead>`
+  for (let [path, count] of pathmap) {
+    pathtable += `<tr>
+    <td>${path}</td>
+    <td>${count}</td>
+      </tr>`
+  }
+  pathtable += '</table>'
+
+  sourcetable += '</table>'
+  return `${pathtable}<br/>${sourcetable}`
 }
+
 
 module.exports = router
