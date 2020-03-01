049943e667ff29ef917e9b1acca3b9d224f62d00
th2 <th2@users.noreply.github.com>
2020-03-01 10:31:42 +0100

    refactor day.js

--- dash/day.js
+++ dash/day.js
@@ -5,24 +5,46 @@ const secrets = require('../config/secrets')
 const router = express.Router()
 
 router.get('/:day', function (req, res) {
-  let page = `<html><head>
-  <title>Dash ${req.params.day}</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="/dash.css">
-  </head><body class=".bg-dark">`
-  
-  const lines = fs.readFileSync(`log/visit/${req.params.day}`).toString().split("\n")
-  let pathmap = new Map()
+  let visits = readVisits(req.params.day)
+  res.send(`
+  <html><head>
+    <title>Dash ${req.params.day}</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="/dash.css">
+  </head><body class=".bg-dark">
+    <div class="row">
+      <div class="col" style="height: 500px; overflow-y: scroll;">
+        ${aggregatePathsTable(visits)}
+      </div>
+      <div class="col">
+        column 2: aggregate user ip
+      </div>
+      <div class="col">
+      column 2: aggregateUserAgent
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        ${visitTable(visits)}
+      </div>
+    </div>
+  </body></html>`)
+})
+
+function readVisits(day) {
+  let visits = []
+  let lines = fs.readFileSync(`log/visit/${day}`).toString().split("\n")
   for (let i in lines) {
     if (lines[i].startsWith('{')) {
-      let visit = JSON.parse(lines[i])
-      let path = visit.meta.req.headers.host + ' ' + visit.message
-      pathmap.set(path, pathmap.get(path) + 1 || 1)
+      visits.push(JSON.parse(lines[i]))
     }
   }
-  let patharray = []
-  pathmap.forEach ((v,k) => { patharray.push({path: k, count: v}) })
-  patharray = patharray.sort((a, b) => b.count - a.count)
+  return visits
+}
+
+function aggregatePathsTable(visits) {
+  let page = ''
+  let aggregation = aggregateCount(visits)
   page += `<table class="table table-dark table-striped">
     <thead>
     <tr>
@@ -30,31 +52,29 @@ router.get('/:day', function (req, res) {
     <th>count</th>
     </tr>
     </thead>`
-  for (let i in patharray) {
+  for (let i in aggregation) {
     page += `<tr>
-    <td>${patharray[i].path}</td>
-    <td>${patharray[i].count}</td>
-      </tr>`
+      <td>${aggregation[i].value}</td>
+      <td>${aggregation[i].count}</td>
+    </tr>`
   }
   page += '</table>'
-  page += '</body></html>'
-  res.send(page)
-})
+  return page
+}
 
+function aggregateCount(visits) {
+  let countMap = new Map()
+  for (let i in visits) {
+    let path = visits[i].meta.req.headers.host + ' ' + visits[i].message
+    countMap.set(path, countMap.get(path) + 1 || 1)
+  }
+  let resultArray = []
+  countMap.forEach ((v,k) => { resultArray.push({value: k, count: v}) })
+  resultArray = resultArray.sort((a, b) => b.count - a.count)
+  return resultArray
+}
 
-router.get('/:day/all', function (req, res) {
-  let page = `<html><head>
-  <title>Dash ${req.params.day}</title>
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <link rel="stylesheet" href="/dash.css">
-  </head><body class=".bg-dark">`
-  page += generateVisitTable(req.params.day)
-  page += '</body></html>'
-  res.send(page)
-})
-
-function generateVisitTable(day) {
-  const lines = fs.readFileSync(`log/visit/${day}`).toString().split("\n")
+function visitTable(visits) {
   let sourcetable = `<table class="table table-dark table-striped"><tr>
     <th>timestamp</th>
     <th>level</th>
@@ -62,24 +82,28 @@ function generateVisitTable(day) {
     <th>meta.req</th>
     <th>meta.res</td>
     </tr>`
-  for (let i in lines) {
-    if (lines[i].startsWith('{')) {
-      let visit = JSON.parse(lines[i])
-      sourcetable += `<tr>
-      <td>${visit.timestamp}</td>
-      <td>${visit.level}</td>
-      <td>${visit.message}</td>
-      <td>${JSON.stringify(visit.meta.req)}</td>
-      <td>${JSON.stringify(visit.meta.res)} responseTime:${visit.meta.responseTime}</td>
-      </tr>`
-    }
+  for (let i in visits) {
+    let visit = visits[i]
+    sourcetable += `<tr>
+    <td>${visit.timestamp}</td>
+    <td>${visit.level}</td>
+    <td>${visit.message}</td>
+    <td>${JSON.stringify(visit.meta.req)}</td>
+    <td>${JSON.stringify(visit.meta.res)} responseTime:${visit.meta.responseTime}</td>
+    </tr>`
   }
-
-
-
   sourcetable += '</table>'
   return `${sourcetable}`
 }
 
+router.get('/:day/all', function (req, res) {
+  let page = `<html><head>
+  <title>Dash ${req.params.day}</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="/dash.css">
+  </head><body class=".bg-dark">`
+  page += '</body></html>'
+  res.send(page)
+})
 
 module.exports = router
