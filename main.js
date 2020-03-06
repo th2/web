f20ca53fd93e178a168e77e1a52aa23af072924d
th2 <th2@users.noreply.github.com>
2020-03-06 20:45:31 +0100

    add https

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'use strict'
 var bodyParser = require('body-parser')
 var express = require('express')
+var fs = require('fs')
+var https = require('https')
 var logger = require('./logger')
 var router = require('./router')
 var config = require('./config/main.json')
+var secrets = require('./config/secrets.json')
 var app = express()
-
 
 app.use(logger.visit())
 app.use(express.static('site'))
@@ -14,6 +16,14 @@ app.use(bodyParser.json())
 app.use('/', router)
 app.use(logger.error())
 
-module.exports.server = app.listen(config.mainPort, function () {
-  console.log('listening on port ' + config.mainPort)
+app.listen(config.httpPort, function () {
+  console.log('http listening on port ' + config.httpPort)
+})
+
+https.createServer({
+  key: fs.readFileSync(secrets.keyPath),
+  cert: fs.readFileSync(secrets.certPath)
+}, app)
+.listen(config.httpsPort, function () {
+  console.log('https listening on port ' + config.httpsPort)
 })
